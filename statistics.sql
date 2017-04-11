1b3b920da52f22b91c3bcb3d28aa477d3fd8facd
beosdev <builhoailinh@dtu.edu.vn>
2017-04-11 14:17:37 +0700

    Update fix Discount SQL

--- statistics.sql
+++ statistics.sql
@@ -19,15 +19,15 @@ CREATE PROC	proc_getSumPriceOfBill
 @dateBegin DATE,@dateEnd DATE 
 AS
 BEGIN
-SELECT 	B.id AS N'ID',B.DateCheckIn AS N'Ngày',SUM(dbo.Food.price * BI.quantity)-B.discount AS N'Thành tiền'
+SELECT 	B.id AS N'ID',B.DateCheckIn AS N'Ngày',SUM(dbo.Food.price * BI.quantity - B.discount) AS N'Thành tiền'
 		
 FROM	dbo.Bill B JOIN dbo.TableFood TB ON B.idTable = tb.id,
 		dbo.BillInfo BI,dbo.Food
 WHERE B.DateCheckIn >= @dateBegin AND B.DateCheckIn <= @dateEnd
 		 AND BI.idBill = B.id  AND BI.idFood = dbo.Food.id
-		 GROUP BY B.id,B.DateCheckIn,B.discount
+		 GROUP BY B.id,B.DateCheckIn
 END
-EXEC dbo.proc_getSumPriceOfBill  @dateBegin = '2017-01-23', @dateEnd = '2017-12-24'
+EXEC dbo.proc_getSumPriceOfBill  @dateBegin = '2017-01-01', @dateEnd = '2017-01-24'
 
 --Lấy doanh thu theo tháng
 DROP	 PROC proc_getSumPriceOfBill
@@ -36,28 +36,28 @@ CREATE PROC	proc_BillMonth
 @month int, @year int
 AS
 BEGIN
-SELECT 	DAY(B.DateCheckIn) AS N'ID',SUM(BI.quantity * dbo.Food.price)-B.discount AS N'Thành tiền'
+SELECT 	DAY(B.DateCheckIn) AS N'ID',SUM(BI.quantity * dbo.Food.price - B.discount) AS N'Thành tiền'
 		
 FROM	dbo.Bill B JOIN dbo.TableFood TB ON B.idTable = tb.id,
 		dbo.BillInfo BI,dbo.Food
 WHERE MONTH(B.DateCheckIn) = @month AND YEAR(B.DateCheckIn) = @year
 		 AND BI.idBill = B.id  AND BI.idFood = dbo.Food.id
-		 GROUP BY DAY(B.DateCheckIn),B.discount
+		 GROUP BY DAY(B.DateCheckIn)
 END
-EXEC dbo.proc_BillMonth @month = N'5', @year = N'2017' -- varchar(10)
-
+EXEC dbo.proc_BillMonth @month = N'1', @year = N'2017' -- varchar(10)
+DROP PROC dbo.proc_BillMonth
 --Lấy doanh thu theo năm
 CREATE PROC	proc_BillYear
 @year int
 AS
 BEGIN
-SELECT 	MONTH(B.DateCheckIn) AS N'ID',SUM(BI.quantity * dbo.Food.price)-B.discount AS N'Thành tiền'
+SELECT 	MONTH(B.DateCheckIn) AS N'ID',SUM(BI.quantity * dbo.Food.price -B.discount) AS N'Thành tiền'
 		
 FROM	dbo.Bill B JOIN dbo.TableFood TB ON B.idTable = tb.id,
 		dbo.BillInfo BI,dbo.Food
 WHERE    YEAR(B.DateCheckIn) = @year
 		 AND BI.idBill = B.id  AND BI.idFood = dbo.Food.id
-GROUP BY MONTH(B.DateCheckIn),B.discount
+GROUP BY MONTH(B.DateCheckIn)
 END
 EXEC dbo.proc_BillYear @year = N'2017' -- varchar(10)
 DROP	PROC proc_BillYear
@@ -68,11 +68,11 @@ CREATE PROC proc_Statistics
 AS
 BEGIN
 	SELECT  BI.idFood AS N'ID',F.name AS N'Tên', SUM(BI.quantity) AS 'Số lượng',
-		 F.price * SUM(BI.quantity) * (100 - BI.discount) AS N'Thành tiền'
+		 F.price * SUM(BI.quantity * (100 - BI.discount)) AS N'Thành tiền'
 	FROM	dbo.BillInfo BI,dbo.Food F,dbo.Bill B
 	WHERE BI.idFood = F.id AND B.id = BI.idBill 
 	AND b.DateCheckIn >= @dateBegin AND b.DateCheckIn <= @dateEnd
-	GROUP BY F.name,f.price,BI.discount,BI.idFood
+	GROUP BY F.name,f.price,BI.idFood
 END
 EXEC dbo.proc_Statistics @dateBegin = '2017-01-23', @dateEnd = '2017-12-23' -- 
 DROP	PROC proc_Statistics 
